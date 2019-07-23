5eee71eb77b9313762e01165ecbb80d81019b903
ygajjar <yogesh@cytel.com>
2019-07-23 16:51:07 -0400

    copied package in main folder

---
+++ src/script.cpp
@@ -0,0 +1,36 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+NumericVector timesTwo(NumericVector x) {
+  return x * 2;
+}
+
+// [[Rcpp::export]]
+double getClosingPrice() { 
+  int days = 1825; // ~ 5 years 
+  NumericVector movement = rnorm(days, 1.001, 0.01); 
+  double closingPrice = sum(movement); 
+  return(closingPrice); 
+}
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+timesTwo(42)
+getClosingPrice()
+*/
